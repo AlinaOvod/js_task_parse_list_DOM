455040beff903728af3b02b3c6fef5b200bef5b7
Alina Ovod <alina.ipatova98@gmail.com>
2024-06-12 13:31:49 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,24 @@
+"use strict";
+const stringToNumber = (salaryString)=>{
+    return Number(salaryString.replace(/[$,]/g, ""));
+};
+const sortList = (list)=>{
+    const sortedList = list.sort((item1, item2)=>stringToNumber(item2.dataset.salary) - stringToNumber(item1.dataset.salary));
+    const listContainer = document.querySelector("ul");
+    sortedList.forEach((item)=>listContainer.append(item));
+};
+const getEmployees = (list)=>{
+    return list.map((item)=>({
+            name: item.textContent,
+            position: item.dataset.position,
+            salary: item.dataset.salary,
+            age: item.dataset.age
+        }));
+};
+const listOfLi = [
+    ...document.querySelectorAll("li")
+];
+sortList(listOfLi);
+getEmployees(listOfLi);
+
+//# sourceMappingURL=index.f75de5e1.js.map
